5801b9f9d556de993b64da6eb6259f8916c11dc3
Elliot McGonigal <elliot@elliotmcgonigal.com>
2021-11-01 20:08:08 -0700

    add calculator functionality for add, subtract, multiply, and divide

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+// Business logic:
+
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+// User interface logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#number1").val());
+    const number2 = parseInt($("#number2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#number1").val());
+    const number2 = parseInt($("#number2").val());
+    const result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#number1").val());
+    const number2 = parseInt($("#number2").val());
+    const result = multiply(number1, number2);
+    $("#output").text(result);
+  });  
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#number1").val());
+    const number2 = parseInt($("#number2").val());
+    const result = divide(number1, number2);
+    $("#output").text(result);
+  });
+});
